5fb394a50c715147e7ca2f24d2ff8ae14927676c
Sujata Kale <49482604+kalesujata97@users.noreply.github.com>
2020-09-29 20:55:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.s3_lambda_trigger.arn
-    events              = ["s3:ObjectCreated:PUT"]
+    events              = ["s3:ObjectCreated:*"]
   }
   depends_on = [aws_lambda_function.s3_lambda_trigger]
 }
