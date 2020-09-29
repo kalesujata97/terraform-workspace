b938d301abbb65d461bcdf9ec643aad5909534d7
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2020-09-29 20:20:28 +0530

    Updated main.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,12 @@ variable "s3_buc_name" {
     type = string
     default = "s3-lambda-trigger-example"
 }
+
+variable "iam_s3_lambda" {
+    type = string
+    default = "s3-lambda-trigger-iam-role"
+}
+variable "dynamo_table_name" {
+    type = string
+    default = "Employee"
+}
