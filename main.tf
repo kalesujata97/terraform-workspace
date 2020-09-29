db2abc3fdd2c25c8cb3c4e5ca32a1514347df0c3
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2020-09-29 20:42:08 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -22,22 +22,80 @@ resource "aws_s3_bucket" "s3_bucket" {
 resource "aws_iam_role" "s3_lambda_iam_role" {
   name = var.iam_s3_lambda
   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
 	{
-	  "Version": "2012-10-17",
-	  "Statement": [
-		{
-		  "Action": "sts:AssumeRole",
-		  "Principal": {
-			"Service": "lambda.amazonaws.com"
-		  },
-		  "Effect": "Allow"
-		}
-	  ]
+	  "Action": "sts:AssumeRole",
+	  "Principal": {
+		"Service": "lambda.amazonaws.com"
+	  },
+	  "Effect": "Allow"
 	}
-	EOF
+  ]
+}
+EOF
 
   tags = {
     "Name" = var.iam_s3_lambda
   }
   depends_on = [aws_s3_bucket.s3_bucket]
 }
+resource "aws_iam_role_policy_attachment" "dynamo-full-policy-attach" {
+  role       = aws_iam_role.s3_lambda_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  depends_on = [aws_iam_role.s3_lambda_iam_role]
+}
+resource "aws_iam_role_policy_attachment" "s3-read-policy-attach" {
+  role       = aws_iam_role.s3_lambda_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  depends_on = [aws_iam_role.s3_lambda_iam_role]
+}
+resource "aws_iam_role_policy_attachment" "lambda-exec-policy-attach" {
+  role       = aws_iam_role.s3_lambda_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  depends_on = [aws_iam_role.s3_lambda_iam_role]
+} 
+
+resource "aws_lambda_function" "s3_lambda_trigger" {
+  filename      = "lambda_function.zip"
+  function_name = "handler"
+  role          = aws_iam_role.s3_lambda_iam_role.arn
+  handler       = "handler.py"
+
+  #source_code_hash = filebase64sha256("lambda_function.zip")
+
+  runtime = "python3.8"
+  
+  depends_on = [aws_iam_role_policy_attachment.lambda-exec-policy-attach]
+}
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_lambda_trigger.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.s3_bucket.arn
+  
+  depends_on = [aws_lambda_function.s3_lambda_trigger]
+} 
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_lambda_trigger.arn
+    events              = ["s3:ObjectCreated:PUT"]
+  }
+  depends_on = [aws_lambda_function.s3_lambda_trigger]
+}
+resource "aws_dynamodb_table" "dynamodb-table" {
+  name           = var.dynamo_table_name
+  hash_key       = "empid"
+  
+  attribute {
+    name = "empid"
+    type = "N"
+  } 
+  depends_on = [aws_s3_bucket_notification.bucket_notification]  
+}
+  
+
