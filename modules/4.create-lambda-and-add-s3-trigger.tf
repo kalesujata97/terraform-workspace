85c0cf04cba38c8e80bccc3a7a9a7abbc45c7cba
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2020-10-01 14:36:43 +0530

    Added api creattion

--- modules/4.create-lambda-and-add-s3-trigger.tf
+++ modules/4.create-lambda-and-add-s3-trigger.tf
@@ -22,5 +22,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.s3_lambda_trigger.arn
     events              = ["s3:ObjectCreated:*"]
+	filter_suffix = ".csv"
   }
 }
